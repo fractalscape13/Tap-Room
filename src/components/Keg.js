e07713727af0329e9c275f77c9ba104025ca2853
Joseph Wangemann <fractalscape13@gmail.com>
2020-04-24 14:06:59 -0700

    add conditional for quantity, update proptypes

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -2,14 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Keg(props) {
+
+  let quantity = '';
+  if (props.quantity > 1) {
+    quantity = props.quantity + ' pints';
+  } else if (props.quantity == 1) {
+    quantity = ' 1 pint left';
+  } else {
+    quantity = "Uh-oh, this tap has run dry. Time to restock!";
+  }
+
   return (
     <React.Fragment>
-      <div onClick = {() => props.whenKegClicked(props.id)}>
+        <p className="clickable" onClick = {() => props.whenKegClicked(props.id)}>On Tap: {props.name} (Click to see details)</p>
+        <p>Quantity in keg: {quantity} </p>
+        <button onClick={()=> props.onClickingSellPint(props.currentKeg) }>Sell a pint</button> 
         <hr />
-        <p className="clickable">On Tap: {props.name}</p>
-        <p>(Click to see details)</p>
-        <hr />
-      </div>
     </React.Fragment>
   );
 }
@@ -17,7 +25,10 @@ function Keg(props) {
 Keg.propTypes = {
   name: PropTypes.string,
   id: PropTypes.string,
-  whenKegClicked: PropTypes.func
+  quantity: PropTypes.number,
+  currentKeg: PropTypes.object,
+  whenKegClicked: PropTypes.func,
+  onClickingSellPint: PropTypes.func
 };
 
 export default Keg;
