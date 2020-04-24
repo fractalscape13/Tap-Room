324d5f3694b5afe5e2f923ae946a60b5c257594d
Joseph Wangemann <fractalscape13@gmail.com>
2020-04-24 10:44:58 -0700

    add methods for handling delete, edit, click, add new keg, also create conditional rendering logic

--- src/components/TapRoomControl.js
+++ src/components/TapRoomControl.js
@@ -25,11 +25,51 @@ class TapRoomControl extends React.Component {
     this.setState({selectedKeg: currentKeg});
   }
 
+  handleClick = () => {
+    if (this.state.selectedKeg != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedKeg: null,
+        editing: false
+      });
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage
+      }));
+    }
+  }
 
+  handleAddingNewKegToList = (newKeg) => {
+    const newMasterKegList = this.state.masterKegList.concat(newKeg);
+    this.setState({masterKegList: newMasterKegList, formVisibleOnPage: false});
+  }
+
+  handleDeletingKeg = (id) => {
+    const newMasterKegList = this.state.masterKegList.filter(keg => keg.id != id);
+    this.setState({masterKegList: newMasterKegList, selectedKeg: null});
+  }
+
+  handleEditingKeg = (kegToEdit) => {
+    const editedMasterKegList = this.state.masterKegList.filter(keg => keg.id != this.state.selectedKeg.id).concat(kegToEdit);
+    this.setState({masterKegList: editedMasterKegList, editing: false, selectedKeg: null});
+  }
 
   render(){
     let currentlyVisibleState = null;
     let buttonText = null;
+    if (this.state.editing) {
+      currentlyVisibleState = <EditKegForm keg={this.state.selectedKeg} onEditKeg={this.handleEditingKeg} />
+      buttonText="Return to Keg List";
+    } else if (this.state.selectedKeg != null) {
+      currentlyVisibleState = <KegDetail keg={this.state.selectedKeg} onClickingDelete={this.handleDeletingKeg} onClickingEdit={this.handleEditingKeg} />
+      buttonText = "Return to Keg List";
+    } else if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewKegForm onNewKegCreation={this.handleAddingNewKegToList} />
+      buttonText = "Return to Keg List";
+    } else {
+      currentlyVisibleState = <KegList kegList={this.state.masterKegList} onKegSelection={this.handleChangingSelectedKeg} />
+      buttonText = "Add a keg";
+    }
     
     return (
       <React.Fragment>
