be911e5ce843873c4931888be2b134ee944ae7ee
Joseph Wangemann <fractalscape13@gmail.com>
2020-04-24 14:06:10 -0700

    update inline styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 function Footer(){
   return (
-    <h3>Zippity doo dah</h3>
+    <div style={style}>
+      <p>People who don't like kombucha just have no.... culture.</p>
+    </div>
   );
+}
+
+const style  = {
+  position: 'fixed',
+  left: 0,
+  bottom: 0,
+  // backgroundColor: 'indigo',
+  // color: 'ivory',
+  width: '100%',
+  display: 'fixed',
+  borderTop: '5px solid black',
+  textAlign: 'center'
 }
 
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 function Header() {
   return (
-    <h1>Booch Palace</h1>
+    <div style={style}>
+      <h1>Booch Palace</h1>
+    </div>
   );
+}
+
+const style  = {
+  display: 'flex',
+  justifyContent: 'center', 
+  margin: 0,
+  width: '100%',
 }
 
 export default Header;
