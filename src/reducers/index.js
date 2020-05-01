8bf57c73069900aecfbf098e16196c7051cdc368
Joseph Wangemann <fractalscape13@gmail.com>
2020-05-01 11:24:38 -0700

    install redux, react-redux, update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import kegListReducer from './keg-list-reducer';
 import toggleReducer from './toggle-reducer';
+import selectReducer from './select-keg-reducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
   toggleConditions: toggleReducer,
-  masterKegList: kegListReducer
+  masterKegList: kegListReducer,
+  selectedKeg: selectReducer
 });
 
 export default rootReducer;
