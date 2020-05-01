ca1af60b54369e8fec6a96d29c3939d3449e8f01
Joseph Wangemann <fractalscape13@gmail.com>
2020-05-01 08:52:17 -0700

    add actiontypes

---
+++ src/actions/ActionTypes.js
@@ -0,0 +1,6 @@
+export const NEW_KEG = 'NEW_KEG';
+export const EDIT_KEG = 'EDIT_KEG';
+export const DELETE_KEG = 'DELETE_KEG';
+export const TOGGLE_FORM = 'TOGGLE_FORM';
+export const TOGGLE_EDITING = 'TOGGLE_EDITING';
+export const SELECT_KEG = 'SELECT_KEG';
