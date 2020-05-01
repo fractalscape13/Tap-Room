d064dc6d21fd7c38547a0aa05a7dc8dc9e0a43d7
Joseph Wangemann <fractalscape13@gmail.com>
2020-05-01 10:00:06 -0700

    split reducer and test files into separate concerns

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,9 +15,32 @@ export const addKeg = (keg) => {
 }
 
 export const editKeg = (keg) => {
-
+  const { name, brand, price, flavor, caffeine, quantity, id } = keg;
+  return {
+    type: c.EDIT_KEG,
+    name: name,
+    brand: brand,
+    price: price,
+    flavor: flavor,
+    caffeine: caffeine,
+    quantity: quantity,
+    id: id
+  }
 }
 
-export const deleteKeg = (id) => {
-  
-}
+export const deleteKeg = (id) => ({
+  type: c.DELETE_KEG,
+  id
+});
+
+export const toggleForm = () => ({
+  type: c.TOGGLE_FORM
+});
+
+export const toggleEditing = () => ({
+  type: c.TOGGLE_EDITING
+});
+
+export const selectKeg = (id) => ({
+  type: c.SELECT_KEG
+});
